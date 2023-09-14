6fcdfcf4bd7d981011692ec168dbf18dc7403e52
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2023-09-14 17:34:42 +0100

    added comment

--- gdb-core-dump/disable_core_dumps.sh
+++ gdb-core-dump/disable_core_dumps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# this doesn't work from inside the script, run directly in terminal
+
 ulimit -S 0
 ulimit -c 0
--- gdb-core-dump/enable_core_dumps.sh
+++ gdb-core-dump/enable_core_dumps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# this doesn't work from inside the script, run directly in terminal
+
 ulimit -S unlimited
 ulimit -c unlimited
