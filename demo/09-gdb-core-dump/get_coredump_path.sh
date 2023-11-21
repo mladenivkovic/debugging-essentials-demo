259b5a21192cbdf5c5449c4e4b2e0c683149dea4
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2023-11-21 02:14:05 +0100

    added gdb core dump

---
+++ demo/09-gdb-core-dump/get_coredump_path.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this is just for the demo. This path is valid for Ubuntu 21+.
+# It may be different on your operating system and machine.
+
+echo Path is
+echo /var/lib/apport/coredump/
+ls /var/lib/apport/coredump/
--- demo/gdb-core-dump/get_coredump_path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo Path is
-echo /var/lib/apport/coredump/
-ls /var/lib/apport/coredump/
