adf43e9f3f4774fba7586648bf9cdc1a9479f312
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2023-11-23 03:13:22 +0000

    fixed cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -3,8 +3,22 @@
 # Do a quick cleanup before the live demo.
 
 cd demo
-for dir in gdb-basics gdb-core-dump gdb-FPE gdb-MPI gdb-optimization gdb-segfault valgrind; do
-
+for dir in \
+    01-gdb-basics \
+    02-gdb-cmdlineargs \
+    03-gdb-breakpoint-function \
+    04-gdb-breakpoint-conditional \
+    05-gdb-reverse \
+    06-gdb-watchpoint \
+    07-gdb-frames \
+    08-gdb-segfault \
+    09-gdb-core-dump \
+    10-gdb-optimization \
+    11-gdb-FPE \
+    12-gdb-MPI \
+    13-valgrind \
+    14-address-sanitizer
+do
     cd $dir
     echo ===================== $PWD
     make clean
